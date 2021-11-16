3690e781f253c60e554bfb7d20062990b85c299e
de-jackson <jackyeyamu@gmail.com>
2021-11-16 15:01:43 +0300

    Solving task 15. Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,4 @@ def remove_char_at(str, n):
     first_part = str[:n]
     last_part = str[n+1:]
     total_part = first_part + last_part
-    if n > 0:
-        return total_part
-    else:
-        return str[0:]
+    return total_part
