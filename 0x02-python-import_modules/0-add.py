2db29752021d1635a2e5e29a70a9c98f804a8095
de-jackson <jackyeyamu@gmail.com>
2021-11-18 16:36:23 +0300

    Solving task 1. My first toolbox!

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,7 +5,8 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    c = add(a, b)
-    print("{} + {} = {}".format(a, b, c))
-    if __name__ == "__main__":
-        main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    main()
