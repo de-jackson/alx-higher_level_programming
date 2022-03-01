a3fe6b3e9cce77e2ea66689afd11362084347a75
de-jackson <jackyeyamu@gmail.com>
2022-03-01 16:28:09 +0300

    Solving task 11. Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
 const myList = require('./100-data').list;
-const mapList = myList.map(function (num, index) {
-	  return num * index;
-});
+const mapList = myList.map((num, index) => num * index);
 console.log(myList);
 console.log(mapList);
